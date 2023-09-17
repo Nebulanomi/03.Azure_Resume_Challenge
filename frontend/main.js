72431f511d6e7e7c186a1d928cc49e6c25577d1a
Alexandre <alexandre.pereira.t0117819@edu.atec.pt>
2023-09-17 13:48:02 +0100

    Updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,22 @@
+///Call function getVisitCount
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+//const = The variable will be the same value forever
+const functionApi = '';
+
+const getVisitCount = () => {
+    //let = The variable will change
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+        }).then(response =>{
+        console.log('Website called function API.');
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+        }).catch(function(error){
+            console.log(error);
+        });
+        return count;
+    }
