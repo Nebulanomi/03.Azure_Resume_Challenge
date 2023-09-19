71c9e26fcd0f2a4f9039f43da43371bac73efcac
Alexandre <alexandre.pereira.t0117819@edu.atec.pt>
2023-09-19 21:20:56 +0100

    Alterations to backend

--- backend/api/Get_Resume_Counter.cs
+++ backend/api/Get_Resume_Counter.cs
@@ -22,7 +22,7 @@ namespace Company.Function
             ILogger log)
         {
 
-            //Here is where the counter gets updated
+            //Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = counter;
