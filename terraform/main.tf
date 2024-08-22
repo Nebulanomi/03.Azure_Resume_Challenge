0a7b8ef17ce4b0e5b696535b0247337a56f874e9
Alexandre Pereira <alexandre.gl.pereira@outlook.pt>
2024-08-22 18:48:01 +0100

    Added new files

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,35 +17,12 @@ resource "random_integer" "ri" {
   max = 99999
 }
 
-resource "azurerm_cosmosdb_account" "example" {
-  name                      = random_pet.prefix.id
-  location                  = var.cosmosdb_account_location
-  resource_group_name       = azurerm_resource_group.example.name
-  offer_type                = "Standard"
-  kind                      = "GlobalDocumentDB"
-  enable_automatic_failover = false
-  enable_free_tier          = true
-  geo_location {
-    location          = var.location
-    failover_priority = 0
-  }
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
-  depends_on = [
-    azurerm_resource_group.example
-  ]
-}
-
 resource "azurerm_cosmosdb_account" "db_account" {
   name                      = "tf-cosmos-account-${random_integer.ri.result}"
   location                  = azurerm_resource_group.rg.location
   resource_group_name       = azurerm_resource_group.rg.name
   kind                      = "GlobalDocumentDB"
   offer_type                = "Standard"
-  enable_automatic_failover = false
   enable_free_tier          = true
   geo_location {
     location          = azurerm_resource_group.rg.location
