70ce4f4d3edb418c65f2aac06f0b5a601e05c38f
Alexandre Pereira <alexandre.gl.pereira@outlook.pt>
2024-08-23 19:17:50 +0100

    Added new files

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,20 +97,19 @@ resource "azurerm_service_plan" "sp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "FunctionApp"
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  sku_name            = "FunSku"
 }
 
-resource "azurerm_function_app" "example" {
+resource "azurerm_function_app" "fun" {
   name                       = "GetResumeCounter"
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.sp.id
+  app_service_plan_id        = azurerm_service_plan.sp.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+
+  os_type = "linux"
+  version = "~3"
 
   site_config {
     dotnet_framework_version = "v6.0"
