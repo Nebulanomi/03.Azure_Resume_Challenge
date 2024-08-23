152f8dd9e15f240dfa66a84e368b0b1f98e67df4
Alexandre Pereira <alexandre.gl.pereira@outlook.pt>
2024-08-23 19:34:31 +0100

    Added new files

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,10 @@ resource "azurerm_service_plan" "sp" {
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Y1"
   os_type             = "Linux"
+
+  application_stack {
+    dotnet_version = "6.0"
+  }
 }
 
 resource "azurerm_linux_function_app" "fun" {
@@ -107,7 +111,6 @@ resource "azurerm_linux_function_app" "fun" {
   service_plan_id            = azurerm_service_plan.sp.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  application_stack {
-    dotnet_version = "6.0"
-  }
+
+  site_config {}
 }
